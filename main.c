a0e7c43262832ff8212f7699bdcd038c0801f24a
Abdul Mumit <mamumit07@gmail.com>
2018-11-30 04:43:20 +0100

    Use CRLF instead of LF

--- main.c
+++ main.c
@@ -3,20 +3,46 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
 #ifndef BACKLOG
 #define BACKLOG 8
 #endif
 
-#ifndef REQUEST_MAX_SIZE
-// Ain't nobody got time for listening to your bullshit!!
-#define REQUEST_MAX_SIZE 128
-#endif
-
 #ifndef DUMB_RESPONSE
 // Show me some love, I could Rick Roll you!
-#define DUMB_RESPONSE "HTTP/1.1 200 OK\nContent-Type: text/html\n\nHi there! I am a dumb web server\n"
+#define DUMB_RESPONSE "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHi there! I am a dumb web server\r\n"
 #endif
+
+#define MAX_HEADER_LEN 128
+#define CONTENT_LENGHT_KEY "Content-Length: "
+
+int parse_content_len(char * buf) {
+  int s = strlen(buf);
+  char key[] = CONTENT_LENGHT_KEY;
+  int k = strlen(key);
+  if (s < k) return 0;
+  if (strncmp(key, buf, k) == 0) {
+    int len = 0;
+    for (int i = k; i < s && buf[i] >= '0' && buf[i] <= '9'; i ++) {
+      len = len*10+buf[i]-'0';
+    }
+    return len;
+  }
+  return 0;
+}
+
+int read_header(int cd) {
+  FILE *f = fdopen(cd, "w+");
+  char * buf = (char*) malloc(MAX_HEADER_LEN * sizeof(char));
+  int len = 0;
+  while ((buf = fgets(buf, MAX_HEADER_LEN, f)) != NULL) {
+    if (strcmp(buf, "\r\n")) break;
+    len = parse_content_len(buf);
+  }
+  return len;
+}
 
 int main() {
   // Heroku needs to get port from env
@@ -51,18 +77,15 @@ int main() {
     int cd = accept(sd, NULL, NULL);
     if (cd < 0) continue;
 
-    // You know what? I'm not gonna check if memory can be allocated or not
-    char * req = (char *) malloc(sizeof(char)*REQUEST_MAX_SIZE);
+    // Some clients don't send EOF
+    // So need to find content length
+    int content_length = read_header(cd);
 
-    // Pfft... kid's stuff
-    int req_len = read(cd, req, REQUEST_MAX_SIZE);
+    // Ignore dat body
+    for (char c; content_length -- > 0; read(cd, &c, 1));
 
-    // Good boy stuff
-    free(req);
-
-    // If you don't talk to me or I can't hear you,
-    // this dumb motherfucker ain't gonna talk
-    if (req_len > 0) write(cd, response, sizeof(response));
+    // dubm response here I come
+    write(cd, response, sizeof(response));
 
     // Good boy stuff again
     close(cd);
